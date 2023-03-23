3ee9ee8b01839047a97f7453bd814728bca0c94f
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2023-03-23 15:20:09 +0200

    update1

--- views/equipment/list-equipment.php
+++ views/equipment/list-equipment.php
@@ -4,20 +4,9 @@ require('../../includes/validations/geart_validation.php');
 require ("../../config/db-config.php");
 ?>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>GearTracker | List Equipment</title>
-      
-      <!-- Favicon -->
-      <link rel="shortcut icon" href="../../assets/images/favicon.png" />
-      <link rel="stylesheet" href="../../assets/css/backend-plugin.min.css">
-      <link rel="stylesheet" href="../../assets/css/backende209.css?v=1.0.0">
-      <link rel="stylesheet" href="../../assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css">
-      <link rel="stylesheet" href="../../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
-      <link rel="stylesheet" href="../../assets/vendor/remixicon/fonts/remixicon.css">
-      <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-  </head>
+    <?php
+  require("../../includes/header.php");
+    ?>
   <body class="  ">
   <!-- loader Start -->
   <div id="loading">
